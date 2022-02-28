a41d64974035ab35d98703faa970ff6f4ef9bdbd
zam0x1ma <56079201+zam0x1ma@users.noreply.github.com>
2022-02-28 14:22:53 -0300

    Add NvChad config

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.options = {
+  relativenumber = true,
+  cursorline = true,
+}
+
+M.ui = {
+   theme = "onedark",
+}
+
+M.plugins = {
+   status = {
+     dashboard = true,
+   },
+}
+
+return M
---
+++ lua/custom/init.lua
@@ -0,0 +1,8 @@
+-- MAPPINGS
+local map = require("core.utils").map
+
+map("n", "<leader>cc", ":Telescope <CR>")
+map("n", "<leader>q", ":q <CR>")
+
+map("n", "<leader>;", "$a;<Esc>")
+map("n", "<leader>,", "$a,<Esc>")
