4ca31d62e849f73ef4c9f0a1c55fee9faeb7882d
zam0x1ma <56079201+zam0x1ma@users.noreply.github.com>
2022-09-16 19:53:47 -0300

    Change .cs files tab space

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -11,4 +11,8 @@ M.ui = {
 
 M.mappings = require "custom.mappings"
 
+M.plugins = {
+  user = require "custom.plugins"
+}
+
 return M
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,1 +1,3 @@
+local autocmd = vim.api.nvim_create_autocmd
 
+autocmd("FileType", { pattern = "cs", command = [[setlocal tabstop=4 shiftwidth=4]] })
